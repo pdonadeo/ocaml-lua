de37a2e09e409316c64e6a0ad0ab9fbc2f06923a
Paolo Donadeo <p.donadeo@ex-nunc.org>
2009-01-11 01:14:31 +0100

    Binding to a simple example found in Programming in Lua 2nd ed., page 219

---
+++ main.ml
@@ -0,0 +1,64 @@
+let (|>) x f = f x
+
+type lua_State
+
+type thread_status =
+  | LUA_OK
+  | LUA_YIELD
+  | LUA_ERRRUN
+  | LUA_ERRSYNTAX
+  | LUA_ERRMEM
+  | LUA_ERRERR
+
+let thread_status_of_int = function
+  | 0 -> LUA_OK
+  | 1 -> LUA_YIELD
+  | 2 -> LUA_ERRRUN
+  | 3 -> LUA_ERRSYNTAX
+  | 4 -> LUA_ERRMEM
+  | 5 -> LUA_ERRERR
+  | _ -> failwith "thread_status_of_int: unknown status value"
+
+exception Lua_error of thread_status
+
+external lua_open : unit -> lua_State = "lua_open__stub"
+external luaL_openlibs : lua_State -> unit = "luaL_openlibs__stub"
+external lua_close : lua_State -> unit = "lua_close__stub"
+external luaL_loadbuffer__wrapper :
+  lua_State -> string -> int -> string -> int = "luaL_loadbuffer__stub"
+
+external lua_pcall__wrapper :
+  lua_State -> int -> int -> int -> int = "lua_pcall__stub"
+
+let luaL_loadbuffer l buff name =
+  luaL_loadbuffer__wrapper l buff (String.length buff) name |>
+    thread_status_of_int
+;;
+
+let lua_pcall l nargs nresults errfunc =
+  lua_pcall__wrapper l nargs nresults errfunc |> thread_status_of_int
+;;
+
+let l = lua_open ();;
+let () = luaL_openlibs l;;
+
+try
+  while true do
+    let line = (read_line ()) ^ "\n" in
+      try
+        match luaL_loadbuffer l line "line" with
+          | LUA_OK -> begin
+              match lua_pcall l 0 0 0 with
+                | LUA_OK -> ()
+                | err -> raise (Lua_error err)
+            end
+          | err -> raise (Lua_error err)
+      with
+        | Lua_error err -> begin
+            Printf.eprintf "%s%!" "ERRORE!!!\n"
+          end
+  done;
+with End_of_file -> ()
+
+let () = lua_close l
+
---
+++ stub.c
@@ -0,0 +1,83 @@
+#include <lua5.1/lua.h>
+#include <lua5.1/lauxlib.h>
+#include <lua5.1/lualib.h>
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+
+/* Encapsulation of opaque Lua state handle (of type lua_State *)
+   as Caml custom blocks. */
+
+static struct custom_operations lua_State_ops =
+{
+  "org.ex-nunc.ocaml_lua",
+  custom_finalize_default,
+  custom_compare_default,
+  custom_hash_default,
+  custom_serialize_default,
+  custom_deserialize_default
+};
+
+
+/* Accessing the lua_State* part of a Caml custom block */
+#define lua_State_val(v) (*((lua_State **) Data_custom_val(v)))
+
+/* Allocating a Caml custom block to hold the given lua_State* */
+static value alloc_lua_State(lua_State *L)
+{
+  value v = alloc_custom(&lua_State_ops, sizeof(lua_State *), 0, 1);
+  lua_State_val(v) = L;
+  return v;
+}
+
+CAMLprim
+value lua_open__stub (value unit)
+{
+  CAMLparam1(unit);
+  CAMLreturn(alloc_lua_State(lua_open()));
+}
+
+CAMLprim
+value luaL_openlibs__stub(value L)
+{
+  CAMLparam1(L);
+  luaL_openlibs(lua_State_val(L));
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim
+value lua_close__stub(value L)
+{
+  CAMLparam1(L);
+  lua_close(lua_State_val(L));
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim
+value luaL_loadbuffer__stub(value L, value buff, value sz, value name)
+{
+  CAMLparam4(L, buff, sz, name);
+  CAMLlocal1(status);
+  status = Val_int(luaL_loadbuffer( lua_State_val(L),
+                                    String_val(buff),
+                                    Int_val(sz),
+                                    String_val(name)) );
+  CAMLreturn(status);
+}
+
+CAMLprim
+value lua_pcall__stub(value L, value nargs, value nresults, value errfunc)
+{
+  CAMLparam4(L, nargs, nresults, errfunc);
+  CAMLlocal1(status);
+  status = Val_int(lua_pcall( lua_State_val(L),
+                              Int_val(nargs),
+                              Int_val(nresults),
+                              Int_val(errfunc)) );
+  CAMLreturn(status);
+}
+
